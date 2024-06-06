ef255239b0fa7887115334e5eea4b4b5d90dbb61
ahmed rakan <ar.aldhafeeri11@gmail.com>
2024-06-06 06:54:00 +0300

    home route need to be fixed, first in the stack page will be visible

--- public/main.py
+++ public/main.py
@@ -28,6 +28,8 @@ router.add_route(Route(
     page=home_page
 ))
 
+router.navigate("/zenaura-landing-page/")
+
 # Run the application
 event_loop = asyncio.get_event_loop()
 event_loop.run_until_complete(router.handle_location())
